6e3e197b6ed456a9bf82ae948e1205029ea3394e
Keer0423 <keerthirajamuri@gmail.com>
2025-08-06 13:22:07 -0400

    updated name

--- src/data/hero.js
+++ src/data/hero.js
@@ -1,6 +1,6 @@
 export const heroData = {
   greeting: "Hi, I'm",
-  name: "Keerthi Reddy Rajamuri",
+  name: "Surya Punna",
   title: "Full Stack Developer",
   subtitle: "I create exceptional digital experiences through clean code and thoughtful design",
   description: "Passionate about building scalable web applications and turning complex problems into simple, beautiful solutions.",
